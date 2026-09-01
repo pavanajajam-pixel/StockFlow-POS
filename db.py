522ecfb155809e48aeec3696a220132e84096074
Jajam Pavana <pavanajajam@gmail.com>
2026-09-01 12:36:49 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,15 +1,17 @@
+import os
 import psycopg2
 
 def connect_db():
     try:
         conn = psycopg2.connect(
-            dbname="stockflow",        # ✅ make sure this database exists
-            user="postgres",        # ✅ your PostgreSQL username
-            password="password",  # 🔑 replace with your actual password
-            host="localhost",
-            port="5432"
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT", "5432")
         )
         return conn
+
     except Exception as e:
         print("Database connection failed:", e)
         return None
