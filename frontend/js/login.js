ed0525150266ef27f31973a223c8becdfb3626eb
Jajam Pavana <pavanajajam@gmail.com>
2026-09-02 12:59:02 +0530

    Update login.js

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -105,7 +105,7 @@ loginForm.addEventListener("submit", async function (event) {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:5000/api/login",
+            "https://stockflow-pos-yorq.onrender.com/api/login",
             {
                 method: "POST",
 
